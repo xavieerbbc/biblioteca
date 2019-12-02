fb36936af925110cacd9e36d39c6b2783be679d4
Jonnathan-Matute <34014602+Jonnathan-Matute@users.noreply.github.com>
2019-12-02 14:42:39 -0500

    Update busqueda

--- biblioteca-master/Biblioteca/biblioteca_server/src/main/java/vista/BusquedaAutoresBean.java
+++ biblioteca-master/Biblioteca/biblioteca_server/src/main/java/vista/BusquedaAutoresBean.java
@@ -1,14 +1,13 @@
 package vista;
 
 import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
-
 import negocio.GestionAutorLocal;
 import negocio.GestionLibrosLocal;
 import modelo.Autor;
 import modelo.Libro;
+
 @ManagedBean
 public class BusquedaAutoresBean {
 	
@@ -17,9 +16,7 @@ public class BusquedaAutoresBean {
 	
 	/*Bean Properties*/
 	private String filtro;
-	
 	private List<Autor> autores;
-	
 	
 	public String BuscarAutor()
 	{
@@ -27,38 +24,28 @@ public class BusquedaAutoresBean {
 		return null;
 	}
 
-
 	public GestionAutorLocal getGl() {
 		return gl;
 	}
-
 
 	public void setGl(GestionAutorLocal gl) {
 		this.gl = gl;
 	}
 
-
 	public String getFiltro() {
 		return filtro;
 	}
-
 
 	public void setFiltro(String filtro) {
 		this.filtro = filtro;
 	}
 
-
 	public List<Autor> getAutores() {
 		return autores;
 	}
 
-
 	public void setAutores(List<Autor> autores) {
 		this.autores = autores;
 	}
-
-	
-
-	
 
 }
